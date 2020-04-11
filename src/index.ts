4a2da05b6c264acecfaf40a8991d70ff0b37ce55
Alex Urdenko <can.nikola@gmail.com>
2020-04-12 00:39:26 +0300

    inited custom element as print form

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hi');
+export class WantedPrintForm extends HTMLElement {
+  constructor() {
+    super();
+
+    const componentShadowRoot = this.attachShadow({ mode: 'open' });
+
+    const template = document.createElement('template');
+    template.innerHTML = 'Wanted serious offender!';
+    componentShadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define('wanted-print-form', WantedPrintForm);
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    library: '@urdenko/print-wanted',
+    libraryTarget: 'commonjs2',
   },
 };
