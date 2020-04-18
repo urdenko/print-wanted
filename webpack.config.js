1e989a3af8a7a18714bd1ebeff85ca3b71efac4b
Alex Urdenko <can.nikola@gmail.com>
2020-04-18 03:24:27 +0300

    It was added dev server for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = {
+const prodConfig = {
   entry: './src/index.ts',
   resolve: {
     extensions: ['.ts'],
@@ -11,4 +11,30 @@ module.exports = {
     library: '@urdenko/print-wanted',
     libraryTarget: 'commonjs2',
   },
+};
+
+const devConfig = {
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: ['src', 'node_modules'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dev'),
+    library: 'printWanted',
+    libraryTarget: 'var',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dev'),
+    port: 8000,
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'production') {
+    return prodConfig;
+  }
+
+  return devConfig;
 };
